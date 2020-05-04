de6aaa8e3928f8b0003e39519a62499d12da454a
Martin Alsinet <martin@alsinet.com.ar>
2020-05-04 15:32:40 -0300

    Agregué configuraciones para Eshell

--- emacs-init.el
+++ emacs-init.el
@@ -72,4 +72,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(term ((t (:inherit default :background "#3F3F3F" :foreground "#DCDCCC")))))
